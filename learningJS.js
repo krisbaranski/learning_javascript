0c6e681b8d21190b7ec4e889f27a48644dbd0551
Krzysztof Baranski <krzysztofbaranski@KrzysztofsMBP2.fritz.box>
2020-11-30 13:23:58 +0100

    Data Types

--- learningJS.js
+++ learningJS.js
@@ -1,32 +1,53 @@
-//  fundamentals 1
+// //  Fundamentals 1
 
-//  linking JavaScript File
-let js = "amazing";
-console.log(40 + 25 + 10 - 5);
+// // linking JavaScript File
+// let js = "amazing";
+// console.log(40 + 25 + 10 - 5);
 
-// Values and Variables
-console.log("Kris");
-console.log(23);
+// // Values and Variables
+// console.log("Kris");
+// console.log(23);
 
-let firstName = "Matilda";
+// let firstName = "Matilda";
 
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+// console.log(firstName);
 
-// Variable name conventions
-let jonas_matilda = "KB";
-let $function = 27;
+// // Variable name conventions
+// let jonas_matilda = "KB";
+// let $function = 27;
 
-let person = "jonas";
-let PI = 3.1415;
+// let person = "jonas";
+// let PI = 3.1415;
 
-let myFirstJob = "Coder";
-let myCurrentJob = "Teacher";
+// let myFirstJob = "Coder";
+// let myCurrentJob = "Teacher";
 
-let job1 = "programmer";
-let job2 = "teacher";
+// let job1 = "programmer";
+// let job2 = "teacher";
 
-console.log(myFirstJob);
+// console.log(myFirstJob);
+
+
+// // Data Types
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+
 
 
