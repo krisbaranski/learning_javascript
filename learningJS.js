45975aa0c53e232575bfd5e8b56782920db53565
Kris Baranski <krisbaranski@gmail.com>
2020-12-03 18:12:46 +0100

    Basic Operations

--- learningJS.js
+++ learningJS.js
@@ -1,39 +1,31 @@
-<<<<<<< HEAD
-// //  Fundamentals 1
-=======
 //  Fundamentals 1
->>>>>>> f021c97a3b1ccf08b27f5dbe6593a825eb858897
+/*
+// linking JavaScript File
+let js = "amazing";
+console.log(40 + 25 + 10 - 5);
 
-// // linking JavaScript File
-// let js = "amazing";
-// console.log(40 + 25 + 10 - 5);
+// Values and Variables
+console.log("Kris");
+console.log(23);
 
-// // Values and Variables
-// console.log("Kris");
-// console.log(23);
+let firstName = "Matilda";
 
-// let firstName = "Matilda";
-
-<<<<<<< HEAD
-// console.log(firstName);
-=======
 console.log(firstName);
->>>>>>> f021c97a3b1ccf08b27f5dbe6593a825eb858897
 
-// // Variable name conventions
-// let jonas_matilda = "KB";
-// let $function = 27;
+// Variable name conventions
+let jonas_matilda = "KB";
+let $function = 27;
 
-// let person = "jonas";
-// let PI = 3.1415;
+let person = "jonas";
+let PI = 3.1415;
 
-// let myFirstJob = "Coder";
-// let myCurrentJob = "Teacher";
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
 
-// let job1 = "programmer";
-// let job2 = "teacher";
+let job1 = "programmer";
+let job2 = "teacher";
 
-// console.log(myFirstJob);
+console.log(myFirstJob);
 
 
 // // Data Types
@@ -56,4 +48,48 @@ year = 1991;
 console.log(typeof year);
 
 console.log(typeof null);
+*/
+// let, const and var
+let age = 30;
+age = 31;
 
+const birthYear = 1991;
+// birthYear = 1990;
+// const job;
+
+var job = 'programmer';
+job = 'teacher'
+
+lastName = 'Schmedtmann';
+console.log(lastName);
+
+// Basic Operators
+// Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'Jonas';
+const lastName = 'Schmedtmann';
+console.log(firstName + ' ' + lastName);
+
+// Assignment operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--;
+x--;
+console.log(x);
+
+// Comparison operators
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
