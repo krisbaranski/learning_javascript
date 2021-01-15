1a984c8b9c43f9e158f6a82846adc6131fca05aa
Kris Baranski <krisbaranski@gmail.com>
2021-01-15 14:27:58 +0100

    values and variables, data types

--- learningJS.js
+++ learningJS.js
@@ -1,4 +1,48 @@
 // Linking a JavaScript File
-// Using browser to test JavaScript code
+// Using browser to test JavaScript
+/*alert("Hello World!")
+
 let js = "amazing";
+
+if (js === 'amazing') alert('JavaScript is FUN!')
+
+js = 'boring'
+
 console.log(40 + 8 + 23 - 10);
+
+
+////////////////////////////////////
+// Values and Variables
+console.log("Kris");
+console.log(41);
+
+let firstName = "Kris";
+console.log(firstName);
+
+let myFirstJob = 'Programmer'
+let myCurrentJob = 'Teacher'
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Kris');
+
+javascriptIsFun = 'YES!';
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1979;
+console.log(typeof year);
+
+// falsy defined typeof
+console.log(typeof null);
+*/
+
+////////////////////////////////////
+// let const var
+
