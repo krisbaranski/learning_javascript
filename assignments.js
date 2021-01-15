4a2875237df5fbf7581dc8e98a3ebd8e8232814f
Kris Baranski <krisbaranski@gmail.com>
2021-01-15 14:23:26 +0100

    assignments exercises

---
+++ assignments.js
@@ -0,0 +1,16 @@
+/*let country = 'Germany';
+let continent = 'Europe';
+let population = '80';
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+*/
