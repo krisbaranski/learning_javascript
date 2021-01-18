62fedc853bfcc7bbf39e2ff91758047c21871969
Kris Baranski <krisbaranski@gmail.com>
2021-01-18 14:32:46 +0100

    basic operators exercise

--- assignments.js
+++ assignments.js
@@ -25,5 +25,44 @@ const country = 'germany';
 const continent = 'europe';
 const isIsland = false;
 isIsland = true;
-*/
 
+const language = 'portuguese';
+const country = 'Portugal';
+const continent = 'Europe';
+let population = 11;
+const isIsland = false;
+*/
+// basic operations
+
+/* my way but compilcated...wrong
+// const population = 80;
+// let halfCountry = population / 2;
+// halfCountry++;
+// console.log(halfCountry);
+
+// const germany = 80;
+// const finnland = 6;
+// const biggerCountry = germany > finnland;
+// console.log(biggerCountry);
+
+// const averagePopulation = 30;
+// console.log(germany > averagePopulation);
+
+
+console.log(population / 2);
+// population++;
+console.log(population);
+console.log(population > 6);
+console.log(population < 33);
+
+
+
+const description1 = country +
+    ' is in ' +
+    continent +
+    ', and its ' +
+    population +
+    ' million people speak ' +
+    language;
+console.log(description1);
+*/
