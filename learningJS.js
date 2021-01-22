a3df29ece9e3032eee20ec655a43c39a53bd337e
Kris Baranski <krisbaranski@gmail.com>
2021-01-22 23:34:53 +0100

    conditional ternary operators, coding challenge

--- learningJS.js
+++ learningJS.js
@@ -371,7 +371,100 @@ if (teamDolphins >= 100 && teamDolphins > teamKoalas) {
 } else {
     console.log("Nobody wins");
 }
-*/
+
 
 ////////////////////////////////////////
 // Switch Statement
+// it is slowly out of use, but is clearer and in some cases,
+// like this down here is better to use switch then if/else
+
+const day = 'wednesday';
+
+switch (day) {
+    case 'monday':  // day === 'monday'
+        console.log('Plan course structure');
+        console.log('Go to coding meetup');
+        break;
+    case 'tuesday':
+        console.log('Prepare theory videos');
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log('Write code examples');
+        break;
+    case 'friday':
+        console.log('Record videos');
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log('Enjoy the weekend');
+        break;
+    default:
+        console.log('Not a valid day');
+}
+
+const day = 'monday';
+
+if (day === 'monday') {
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+} else if (day === 'tuesday') {
+    console.log('Prepare theory videos');
+} else if (day === 'wednesday' || day === 'thursday') {
+    console.log('Write code examples');
+} else if (day === 'friday') {
+    console.log('Record videos');
+} else if (day === 'saturday' || day === 'sunday') {
+    console.log('Enjoy the weekend');
+} else {
+    console.log('Not a valid day');
+}
+
+
+//////////////////////////////////////
+// Statements & Expressions
+
+3 + 4
+1991
+true && false && !false
+
+if (23 > 10) {
+    const str = '23 is bigger';
+}
+const me = 'Jonas';
+console.log(`I'm ${2037 - 1991} years old ${me}`);
+
+
+/////////////////////////////////////////////
+// The Conditional (Ternary) Operator
+
+const age = 23;
+age >= 18 ? console.log('I like to drink wine') :
+    console.log('I like to drink water');
+
+const drink = age >= 18 ? 'wine' : 'water';
+console.log(drink);
+
+let drink2;
+
+if (age >= 18) {
+    drink2 = 'wine';
+} else {
+    drink2 = 'water';
+}
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? 'wine' : 'water'}`);
+
+
+/////////////////////////////////////////
+// Coding Challenge #4
+
+// const bill = 275;
+// const bill = 40;
+const bill = 430;
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
+*/
