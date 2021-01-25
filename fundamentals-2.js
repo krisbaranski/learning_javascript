44d8d9385dbb5e8678d15cfec976974434a8dd13
Kris Baranski <krisbaranski@gmail.com>
2021-01-25 21:37:11 +0100

    functions

--- fundamentals-2.js
+++ fundamentals-2.js
@@ -13,21 +13,30 @@ if (hasDriversLicense) console.log('I can drive');
 // const interface = 'Audio'; 'interface' reserved as defined variables and can't be used
 // const private = 345;      'private' reserved as defined variables and can't be used
 // const if;            'if' reserved as defined variables and can't be used
-*/
 
-function logger() {
+//////////////////////////////
+// FUNCTIONS
+
+function logger() { // logger is a name of function
     console.log('My name is Jonas');
 }
 // calling / running / invoking function
 logger();
 logger();
 logger();
-
+*/
 function fruitProccesor(apples, oranges) {
     console.log(apples, oranges)
     const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
     return juice;
 }
-const appleJuice = fruitProccesor(5, 0);
-console.log(appleJuice);
-console.log(fruitProccesor(5, 0));
+const appleJuice = fruitProccesor(5, 0);   // this is one
+console.log(appleJuice);                   // possibility;
+console.log(fruitProccesor(5, 0));    //this is another posibility
+
+const appleOrangeJuice = fruitProccesor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number('23');
+
+
