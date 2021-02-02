0d584f317c786da9ebdecaaabaa489393afaaae6
Kris Baranski <krisbaranski@gmail.com>
2021-02-02 11:29:04 +0100

    basic array operations (methods)

--- fundamentals-2.js
+++ fundamentals-2.js
@@ -239,6 +239,7 @@ console.log(friends.indexOf('Steven'));
 console.log(friends.indexOf('Bob'));
 
 friends.push(23);
+console.log(friends);
 console.log(friends.includes('Steven'));
 console.log(friends.includes('Bob'));
 console.log(friends.includes('23')); //string is not included
