1860a73eb74da07b77231f7c8a24ea20e6446021
Kris Baranski <krisbaranski@gmail.com>
2021-02-03 17:10:10 +0100

    coding challenge

--- fundamentals-2.js
+++ fundamentals-2.js
@@ -213,7 +213,7 @@ console.log(age1, age2, age3);
 const ages = [calcAge2(years[0]), calcAge2(years[1]), calcAge2(years.length - 1)];
 console.log(ages);
 
-*/
+
 
 // Basic Array Operations (Methods)
 const friends = ['MIchael', 'Steven', 'Peter'];
@@ -251,5 +251,23 @@ if (friends.includes('Steven')) {
 
 
 // Coding Challenge #2
+// my solution
+// let bill = 100;
+// const calcTip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// const totalValue = calcTip + bill;
+// console.log(totalValue);
 
+
+const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(bills, tips, total);
+*/
+
+
+// Objects
 
