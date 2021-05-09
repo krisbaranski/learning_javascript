aa61ea7bdd81235a11704adea0a62d2e8f7f0118
Krzysztof Baranski <krisbaranski@gmail.com>
2021-05-09 18:33:49 +0200

    challenge 1

--- structures_operators_strings/script.js
+++ structures_operators_strings/script.js
@@ -61,6 +61,150 @@ restaurant.orderDelivery({
   address: 'Via del Sol',
   starterIndex: 1,
 });
+//
+//
+//
+//
+///////////////////////////////////////
+// Looping arrays for-of
+///////////////////////////////////////
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+for (const items of menu) console.log(items);
+//
+//
+//
+//
+///////////////////////////////////////
+// Coding Challenge #1
+///////////////////////////////////////
+
+/* 
+We're building a football betting app (soccer for my American friends 😅)!
+
+Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
+
+1. Create one player array for each team (variables 'players1' and 'players2')
+2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+3. Create an array 'allPlayers' containing all players of both teams (22 players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+
+GOOD LUCK 😀
+*/
+// const game = {
+//   team1: 'Bayern Munich',
+//   team2: 'Borrussia Dortmund',
+//   players: [
+//     [
+//       'Neuer',
+//       'Pavard',
+//       'Martinez',
+//       'Alaba',
+//       'Davies',
+//       'Kimmich',
+//       'Goretzka',
+//       'Coman',
+//       'Muller',
+//       'Gnarby',
+//       'Lewandowski',
+//     ],
+//     [
+//       'Burki',
+//       'Schulz',
+//       'Hummels',
+//       'Akanji',
+//       'Hakimi',
+//       'Weigl',
+//       'Witsel',
+//       'Hazard',
+//       'Brandt',
+//       'Sancho',
+//       'Gotze',
+//     ],
+//   ],
+//   score: '4:0',
+//   scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+//   date: 'Nov 9th, 2037',
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
+
+// // Coding challenge
+// //
+// // 1.
+// const [players1, players2] = game.players; // object.element extracted/created two new arrays out of one array element
+// console.log(players1, players2);
+
+// // 2.
+// const [gk, ...fieldPlayers] = players1; // first element separated from ...rest elements
+// console.log(gk, fieldPlayers);
+
+// // 3.
+// const allPlayers = [...players1, ...players2]; // adding two object.elements together
+// console.log(allPlayers);
+
+// // 4.
+// const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic']; // adding new elements to end of existing array
+// console.log(players1Final);
+
+// // 5.
+// const { team1, x: draw, team2 } = game.odds; // changing name of element inside an object
+// console.log(game.odds);
+// // or
+// // const {
+// //     odds: { team1, x: draw, team2 },
+// //   } = game;
+// //   console.log(team1, draw, team2);
+
+// // 6.
+// const printGoals = function (...players) {
+//   console.log(players);
+//   console.log(`${players.length} goals  were scored`);
+// };
+
+// printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+// // is printing message with number of goals (4)
+// printGoals('Davies', 'Muller');
+// // is printing message with number of goals (2)
+// printGoals(...game.scored);
+// // is printing number of goals and array of players
+// //
+
+// // printGoals: function (players = [], scored = []) {
+// //     console.log(game.players);
+// //     console.log(game.scored);
+// //   },
+// // const playersScored = [...allPlayers, ...game.scored];
+// // console.log(playersScored);
+// // game.printGoals(...game.players, ...game.scored);
+
+// // 7. // short circuiting instead of if/else
+// // if team1 is smaller than team2 && is more likely to win, so if both is true, clg will print message
+// team1 < team2 && console.log('Team1 is more likely to win');
+// team1 > team2 && console.log('Team2 is more likely to win');
+
+//
+//
+//
+//
+// //
+// // // ////////////////////////
+// // // NULLISH (??)
+// // // ////////////////////////
+// restaurant.numGuests = 0;
+// const guests = restaurant.numGuests || 10;
+// console.log(guests);
+
+// // NULLISH values: undefined, null, but NOT (0, '')
+// const guestsCorrect = restaurant.numGuests ?? 10;
+// console.log(guestsCorrect);
 
 //
 //
@@ -94,7 +238,7 @@ restaurant.orderDelivery({
 // console.log('Hello' && 23 && null && 'Jonas'); // not al truthy, then takes  first falsy
 
 // // practical example
-// // long way
+// long way
 // if (restaurant.orderPizza) {
 //   restaurant.orderPizza('mushrooms', 'spinach');
 // }
@@ -126,7 +270,7 @@ restaurant.orderDelivery({
 
 // // Functions
 // const add = function (...numbers) {
-//   //   console.log(numbers); //out values into arrays
+//   //   console.log(numbers); // put values into arrays
 //   let sum = 0;
 //   for (let i = 0; i < numbers.length; i++) sum += numbers[i];
 //   console.log(sum);
@@ -141,6 +285,7 @@ restaurant.orderDelivery({
 
 // restaurant.orderPizza('mushroom', 'onion', 'olives', 'spinach'); // only mushrooms are mainIngredient, rest is i new array
 // restaurant.orderPizza('mushrooms'); // will give main ingredient and empty array
+
 //
 //
 //
@@ -210,6 +355,7 @@ restaurant.orderDelivery({
 // const { name, openingHours, categories } = restaurant;
 // console.log(name, openingHours, categories);
 
+// renaming elements
 // const {
 //   name: restaurantName,
 //   openingHours: hours,
