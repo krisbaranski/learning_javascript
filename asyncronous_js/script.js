20d8dec684f4ed4025c1ed549c24e652601ad2ec
Krzysztof Baranski <krisbaranski@gmail.com>
2021-05-26 17:03:58 +0200

    first request, second request, callback hell

--- asyncronous_js/script.js
+++ asyncronous_js/script.js
@@ -7,36 +7,119 @@ const countriesContainer = document.querySelector('.countries');
 // Old version of making request
 ///////////////////////////////////////
 
-const getCountryData = function (country) {
-  const request = new XMLHttpRequest();
+// const getCountryData = function (country) {
+//   const request = new XMLHttpRequest();
 
-  request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
-  request.send();
+//   request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
+//   request.send();
 
-  request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
+//   request.addEventListener('load', function () {
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
 
-    const html = `
-  <article class="country">
-  <img class="country__img" src="${data.flag}" />
-  <div class="country__data">
-    <h3 class="country__name">${data.name}</h3>
-    <h4 class="country__region">${data.region}</h4>
-    <p class="country__row"><span>👫</span>${(
-      +data.population / 1000000
-    ).toFixed(1)} people</p>
-    <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-    <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-  </div>
-</article>
-`;
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
-  });
-};
+//     const html = `
+//   <article class="country">
+//   <img class="country__img" src="${data.flag}" />
+//   <div class="country__data">
+//     <h3 class="country__name">${data.name}</h3>
+//     <h4 class="country__region">${data.region}</h4>
+//     <p class="country__row"><span>👫</span>${(
+//       +data.population / 1000000
+//     ).toFixed(1)} people</p>
+//     <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+//     <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+//   </div>
+// </article>
+// `;
+//     countriesContainer.insertAdjacentHTML('beforeend', html);
+//     countriesContainer.style.opacity = 1;
+//   });
+// };
 
-getCountryData('poland');
-getCountryData('germany');
-getCountryData('usa');
-getCountryData('portugal');
+// getCountryData('poland');
+// getCountryData('germany');
+// getCountryData('usa');
+
+///////////////////////////////////////
+// Second call - first request, than second depending of first
+///////////////////////////////////////
+
+// const renderCountry = function (data, className) {
+//   const html = `
+//   <article class="country ${className} ">
+//   <img class="country__img" src="${data.flag}" />
+//   <div class="country__data">
+//     <h3 class="country__name">${data.name}</h3>
+//     <h4 class="country__region">${data.region}</h4>
+//     <p class="country__row"><span>👫</span>${(
+//       +data.population / 1000000
+//     ).toFixed(1)} people</p>
+//     <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+//     <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+//   </div>
+// </article>
+// `;
+//   countriesContainer.insertAdjacentHTML('beforeend', html);
+//   countriesContainer.style.opacity = 1;
+// };
+
+// const getCountryAndNeighbour = function (country) {
+//   // AJAX call country 1
+//   const request = new XMLHttpRequest();
+
+//   request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
+//   request.send();
+
+//   request.addEventListener('load', function () {
+//     // here we destructure array cause of 'name' API
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
+
+//     // render country 1
+//     renderCountry(data);
+
+//     // get Neighbour data
+//     const [neighbour] = data.borders;
+
+//     if (!neighbour) return;
+
+//     // AJAX call country 2
+//     const request2 = new XMLHttpRequest();
+
+//     request2.open('GET', `https://restcountries.eu/rest/v2/alpha/${neighbour}`);
+//     request2.send();
+
+//     request2.addEventListener('load', function () {
+//       // if not name but alpha(code) then will not destructure into array!!!
+//       const data2 = JSON.parse(this.responseText);
+//       console.log(data2);
+//       renderCountry(data2, 'neighbour');
+//     });
+//   });
+// };
+
+// getCountryAndNeighbour('portugal');
+
+///////////////////////////////////////
+// typical callback hell
+///////////////////////////////////////
+
+// setTimeout(() => {
+//   console.log('1 second passed');
+//   setTimeout(() => {
+//     console.log('2 seconds passed');
+//     setTimeout(() => {
+//       console.log('3 seconds passed');
+//       setTimeout(() => {
+//         console.log('4 seconds passed');
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
+
+///////////////////////////////////////
+// Fetch
+///////////////////////////////////////
+
+const request3 = fetch('https://restcountries.eu/rest/v2/name/portugal');
+console.log(request3);
