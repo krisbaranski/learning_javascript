4a9e2c1890eb44e12aab5480beab11f45c1a353f
Krzysztof Baranski <krisbaranski@gmail.com>
2021-05-31 18:52:03 +0200

    update js file

--- asyncronous_js/script.js
+++ asyncronous_js/script.js
@@ -44,24 +44,24 @@ const countriesContainer = document.querySelector('.countries');
 // Second call - first request, than second depending of first
 ///////////////////////////////////////
 
-// const renderCountry = function (data, className) {
-//   const html = `
-//   <article class="country ${className} ">
-//   <img class="country__img" src="${data.flag}" />
-//   <div class="country__data">
-//     <h3 class="country__name">${data.name}</h3>
-//     <h4 class="country__region">${data.region}</h4>
-//     <p class="country__row"><span>👫</span>${(
-//       +data.population / 1000000
-//     ).toFixed(1)} people</p>
-//     <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-//     <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-//   </div>
-// </article>
-// `;
-//   countriesContainer.insertAdjacentHTML('beforeend', html);
-//   countriesContainer.style.opacity = 1;
-// };
+const renderCountry = function (data, className) {
+  const html = `
+  <article class="country ${className} ">
+  <img class="country__img" src="${data.flag}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(
+      +data.population / 1000000
+    ).toFixed(1)} people</p>
+    <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+    <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+  </div>
+</article>
+`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
 
 // const getCountryAndNeighbour = function (country) {
 //   // AJAX call country 1
@@ -121,5 +121,12 @@ const countriesContainer = document.querySelector('.countries');
 // Fetch
 ///////////////////////////////////////
 
-const request3 = fetch('https://restcountries.eu/rest/v2/name/portugal');
-console.log(request3);
+// const request3 = fetch('https://restcountries.eu/rest/v2/name/portugal');
+// console.log(request3);
+
+const getCountryData = function (country) {
+  fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+    .then(response => response.json())
+    .then(data => renderCountry(data[0]));
+};
+getCountryData('portugal');
